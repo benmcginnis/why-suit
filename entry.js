ed02c1d174f36e40c7f004cf8e27b7143762228d
Ben McGinnis <bmcginnis@yext.com>
2017-04-12 10:33:43 -0400

    mostly ready to go

--- entry.js
+++ entry.js
@@ -1,7 +1,10 @@
+require('!style-loader!css-loader!./node_modules/highlightjs/styles/mono-blue.css');
 require('!style-loader!css-loader!./node_modules/reveal.js/css/reveal.css');
 require(
   '!style-loader!css-loader!./node_modules/reveal.js/css/theme/white.css',
 );
+import Highlight from 'highlightjs/highlight.pack.js';
+Highlight.initHighlightingOnLoad();
 import Reveal from 'reveal.js';
 const thanksMD = require('./slides/thank-you.md');
 const thanks = require('./templates/thank-you.soy').thankYouList;
